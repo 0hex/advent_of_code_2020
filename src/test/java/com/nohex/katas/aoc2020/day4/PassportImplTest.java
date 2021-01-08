c90d76a1d5fccddf4575f5e1137858511915f77d
Max Noé <max.noe@dynatrace.com>
2021-01-08 16:32:58 +0100

    Fix day 4 part 2 tests

--- src/test/java/com/nohex/katas/aoc2020/day4/PassportImplTest.java
+++ src/test/java/com/nohex/katas/aoc2020/day4/PassportImplTest.java
@@ -13,13 +13,13 @@ class PassportImplTest {
   @Test
   void given_theRequiredFields_when_aNewPassportIsRequested_then_aNewPassportIsCreated() {
     Map<String, String> fields = new HashMap<>();
-    fields.put("byr", "");
-    fields.put("iyr", "");
-    fields.put("eyr", "");
-    fields.put("hgt", "");
-    fields.put("hcl", "");
-    fields.put("ecl", "");
-    fields.put("pid", "");
+    fields.put("byr", "1970");
+    fields.put("iyr", "2010");
+    fields.put("eyr", "2020");
+    fields.put("hgt", "180cm");
+    fields.put("hcl", "#000000");
+    fields.put("ecl", "hzl");
+    fields.put("pid", "012345678");
 
     assertThatNoException().isThrownBy(() -> new PassportImpl(fields));
   }
@@ -27,38 +27,40 @@ class PassportImplTest {
   @Test
   void given_requiredFieldsAreMissing_when_aNewPassportIsRequested_then_aNewPassportIsNotCreated() {
     Map<String, String> fields = new HashMap<>();
-    fields.put("byr", "");
     assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
 
-    fields.put("iyr", "");
+    fields.put("byr", "1970");
     assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
 
-    fields.put("eyr", "");
+    fields.put("iyr", "2010");
     assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
 
-    fields.put("hgt", "");
+    fields.put("eyr", "2020");
     assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
 
-    fields.put("hcl", "");
+    fields.put("hgt", "180cm");
     assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
 
-    fields.put("ecl", "");
+    fields.put("hcl", "#000000");
     assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
 
-    fields.put("pid", "");
+    fields.put("ecl", "hzl");
+    assertThatIllegalStateException().isThrownBy(() -> new PassportImpl(fields));
+
+    fields.put("pid", "123456789");
     assertThatNoException().isThrownBy(() -> new PassportImpl(fields));
   }
 
   @Test
   void given_aNewPassport_when_cidIsNotSpecified_then_passportHasNoCid() {
     Map<String, String> fields = new HashMap<>();
-    fields.put("byr", "");
-    fields.put("iyr", "");
-    fields.put("eyr", "");
-    fields.put("hgt", "");
-    fields.put("hcl", "");
-    fields.put("ecl", "");
-    fields.put("pid", "");
+    fields.put("byr", "1970");
+    fields.put("iyr", "2010");
+    fields.put("eyr", "2020");
+    fields.put("hgt", "180cm");
+    fields.put("hcl", "#000000");
+    fields.put("ecl", "hzl");
+    fields.put("pid", "012345678");
 
     Passport sut = new PassportImpl(fields);
     assertThat(sut.getCountryId()).isEmpty();
@@ -67,14 +69,14 @@ class PassportImplTest {
   @Test
   void given_aPassport_when_cidIsSpecified_then_passportHasCid() {
     Map<String, String> fields = new HashMap<>();
-    fields.put("byr", "");
-    fields.put("iyr", "");
-    fields.put("eyr", "");
-    fields.put("hgt", "");
-    fields.put("hcl", "");
-    fields.put("ecl", "");
-    fields.put("pid", "");
-    fields.put("cid", "");
+    fields.put("byr", "1970");
+    fields.put("iyr", "2010");
+    fields.put("eyr", "2020");
+    fields.put("hgt", "180cm");
+    fields.put("hcl", "#000000");
+    fields.put("ecl", "hzl");
+    fields.put("pid", "012345678");
+    fields.put("cid", "123");
 
     Passport sut = new PassportImpl(fields);
     assertThat(sut.getCountryId()).isPresent();
