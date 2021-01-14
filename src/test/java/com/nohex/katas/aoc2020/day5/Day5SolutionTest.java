4d54cae22e8e76cfb25fb456eb3eff43f60195e2
Max Noé <max.noe@dynatrace.com>
2021-01-14 10:59:45 +0100

    Day 5, part 2.

--- src/test/java/com/nohex/katas/aoc2020/day5/Day5SolutionTest.java
+++ src/test/java/com/nohex/katas/aoc2020/day5/Day5SolutionTest.java
@@ -6,8 +6,12 @@ import com.nohex.katas.aoc2020.SolutionTest;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
@@ -20,9 +24,7 @@ class Day5SolutionTest extends SolutionTest {
       throws IOException, URISyntaxException {
     Stream<String> input = getResourceAsStream("day5/example.txt");
 
-    Optional<Integer> maxId = input
-        .map(SeatImpl::new)
-        .map(SeatImpl::getId)
+    Optional<Integer> maxId = getSeatIds(input)
         .max(Comparator.naturalOrder());
 
     assertThat(maxId)
@@ -37,14 +39,44 @@ class Day5SolutionTest extends SolutionTest {
     // Load input from file.
     Stream<String> input = getResourceAsStream("day5/input.txt");
 
-    Optional<Integer> maxId = input
-        .map(SeatImpl::new)
-        .map(SeatImpl::getId)
+    Optional<Integer> maxId = getSeatIds(input)
         .max(Comparator.naturalOrder());
 
     assertThat(maxId).isPresent();
 
     logger.info("The result is " + maxId.get());
+  }
+
+  @Test
+  void given_theDay5Input_when_theSeatListIsProduced_then_theMissingSeatIsWhatRemains()
+      throws IOException, URISyntaxException {
+    // Load input from file.
+    Stream<String> input = getResourceAsStream("day5/input.txt");
+
+    // Collect the seat ID list.
+    List<Integer> seatIds = getSeatIds(input)
+        .sorted()
+        .collect(Collectors.toList());
+
+    // Find an ID missing from the list, whose previous and next IDs are in the list.
+    int maxRows = 128;
+    int maxColumns = 8;
+    int numSeats = (maxRows * 8) + maxColumns;
+    OptionalInt mySeatId = IntStream.range(0, numSeats)
+        .filter(i -> !seatIds.contains(i))
+        .filter(i -> seatIds.contains(i - 1))
+        .filter(i -> seatIds.contains(i + 1))
+        .findFirst();
+
+    assertThat(mySeatId).isPresent();
+
+    logger.info("The result is " + mySeatId.getAsInt());
+  }
+
+  private Stream<Integer> getSeatIds(Stream<String> input) {
+    return input
+        .map(SeatImpl::new)
+        .map(SeatImpl::getId);
   }
 
 }
